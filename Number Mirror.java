1cd1cdfba3920978320b50a2a1ec2ead20f306ca
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-04-09 20:07:17 +0530

    Create  Number Mirror.java

---
+++ Number Mirror.java
@@ -0,0 +1,23 @@
+/*problem : https://www.codechef.com/LP0TO101/problems/START01
+  Number Mirror.java
+  Problem Code:START01
+  Status:
+         unattempted 
+*/
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		int N=sc.nextInt();
+		System.out.println(N);
+	}
+}
